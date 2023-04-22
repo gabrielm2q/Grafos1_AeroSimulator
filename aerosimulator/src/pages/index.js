5c8dab544badc0ebe931ab4096612cb4682a2b9f
gabrielm2q <mariano.gabriel2q@gmail.com>
2023-04-22 20:14:03 -0300

    :sparkles: evoluindo o mapa com outra biblioteca (em progresso)

--- aerosimulator/src/pages/index.js
+++ aerosimulator/src/pages/index.js
@@ -1,18 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { INITIAL_VALUE, TOOL_NONE, UncontrolledReactSVGPanZoom } from 'react-svg-pan-zoom';
-import { ReactSvgPanZoomLoader } from 'react-svg-pan-zoom-loader';
+import React, { useRef } from 'react';
+import ReactPanZoom from 'react-image-pan-zoom-rotate';
 import logo from '../assets/logo.png';
 import mapa from '../assets/mapa.svg';
 
 const Aero = () => {
     const Viewer = useRef();
-
-    const [tool, setTool] = useState(TOOL_NONE);
-    const [value, setValue] = useState(INITIAL_VALUE);
-
-    useEffect(() => {
-        Viewer.current.fitToViewer();
-    }, []);
 
     return (
         <div class="flex-container">
@@ -20,34 +12,12 @@ const Aero = () => {
                 <img src={logo}></img>
             </div>
             <div class="mapa">
-                <ReactSvgPanZoomLoader
-                    src={mapa}
-                    render={(graph) => (
-                        <UncontrolledReactSVGPanZoom
-                            ref={Viewer}
-                            width={1500}
-                            height={1500}
-                            tool={tool}
-                            setTool={setTool}
-                            value={value}
-                            setValue={setValue}
-                            scaleFactorMin={1}
-                            scaleFactorMax={5}
-                            background='#F1FAEE'
-                            SVGBackground='#F1FAEE'
-                            customMiniature={() => null}
-                        >
-                            <svg width={500} height={500}>
-                                {graph}
-                            </svg>
-                        </UncontrolledReactSVGPanZoom>
-                    )}
+                <ReactPanZoom
+                    image={mapa}
+                    alt="GRAFO"
                 />
-                
             </div>
         </div>
-        
-        
     );
 }
 
